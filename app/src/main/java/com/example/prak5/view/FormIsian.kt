c4d2f3ede9ec0913edd6d242519a2238ad84f72a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 14:20:01 +0700

    Menambahkan daftar gender dan status perkawinan serta warna background

--- app/src/main/java/com/example/prak5/view/FormIsian.kt
+++ app/src/main/java/com/example/prak5/view/FormIsian.kt
@@ -2,6 +2,7 @@ package com.example.prak5.view
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormPendaftaran(
@@ -12,4 +13,10 @@ fun FormPendaftaran(
     var textAlamat by remember { mutableStateOf("") }
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
+
+    val genderList = listOf("Laki-laki", "Perempuan")
+    val statusList = listOf("Janda", "Lajang", "Duda")
+
+    val backgroundLavender = Color(0xFFF3E5F5)
+    val purpleButton = Color(0xFF7C3AED)
 }
