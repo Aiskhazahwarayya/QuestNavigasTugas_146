0df63fa6c19e931b6bd0a93170b844b8948374ce
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 16:19:37 +0700

    menambahkan Column dengan background Lavender dan padding Scaffold

--- app/src/main/java/com/example/prak5/view/TampilData.kt
+++ app/src/main/java/com/example/prak5/view/TampilData.kt
@@ -1,5 +1,10 @@
 package com.example.prak5.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +14,8 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,7 +48,15 @@ fun TampilData(
                 )
             )
         }
-    ){
+    ){ paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Lavender)
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){}
 
     }
 }
