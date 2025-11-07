d1c663c97da482d56daf9729bf803f665d27fe69
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 15:53:50 +0700

    Membuat struktur dasar Composable WelcomeScreen

---
+++ app/src/main/java/com/example/prak5/view/WelcomeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.cn.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun WelcomeScreen(
+    onSubmitClick: () -> Unit
+) {
+    val colorBackground = Color(0xFFE6E6FA)
+    val colorPurple700 = Color(0xFF673AB7)
+    val colorDeepPurple = Color(0xFF512DA8)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorBackground)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
