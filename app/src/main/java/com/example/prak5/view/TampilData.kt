063379aeb5288908d09ef80b938ff1b92d1ec706
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 16:17:22 +0700

    menambahkan TopAppBar pada Scaffold dengan judul "List Daftar Peserta"

--- app/src/main/java/com/example/prak5/view/TampilData.kt
+++ app/src/main/java/com/example/prak5/view/TampilData.kt
@@ -1,8 +1,16 @@
 package com.example.prak5.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     namaLengkap: String,
@@ -17,4 +25,23 @@ fun TampilData(
     val LightPurple = Color(0xFFFFFFFF)
     val DeepPurple = Color(0xFF7C3AED)
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "List Daftar Peserta",
+                        color = Color.White,
+                        fontSize = 35.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Purple500
+                )
+            )
+        }
+    ){
+
+    }
 }
